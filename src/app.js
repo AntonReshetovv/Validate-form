104b1aa6e23df067e9b1c58b95e69f82e7b39d7b
Anton <Reshetov190991@mail.ru>
2021-04-01 20:24:50 +0700

    add validate for form

--- src/app.js
+++ src/app.js
@@ -1,3 +1,24 @@
-import './css/style.css';
+import { isValidate, isValidPassword, isValidEmail, isValidConfirmPassword } from './utils'
+import './css/style.css'
 
-console.log('app working!')
+const form = document.getElementById('form')
+const email = document.getElementById('inputEmail')
+const password = document.getElementById('inputPassword')
+const confirmPassword = document.getElementById('inputConfirmPassword')
+
+email.addEventListener('blur', () => {
+    isValidEmail(email)
+})
+
+password.addEventListener('blur', () => {
+    isValidPassword(password)
+})
+
+confirmPassword.addEventListener('blur', () => {
+    isValidConfirmPassword(password, confirmPassword)
+})
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    isValidate(email, password, confirmPassword)
+})
---
+++ src/utils.js
@@ -0,0 +1,39 @@
+
+const regExpEmail = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i
+const regExpPassword = /^.{6,}$/
+
+export function isValidate(email, password, confirmPassword) {
+    isValidEmail(email)
+    isValidPassword(password)
+    isValidConfirmPassword(password, confirmPassword)
+}
+
+export function isValidEmail(email) {
+    if (email.value === '') {
+        email.nextElementSibling.textContent = "Необходимо заполнить «Электронную почту»"
+    } else if (!regExpEmail.test(email.value)) {
+        email.nextElementSibling.textContent = 'Значение «Электронной почты» не является правильным адресом «Электронной почты»'
+    } else {
+        email.nextElementSibling.textContent = ""
+    }
+}
+
+export function isValidPassword(password) {
+    if (password.value === '') {
+        password.nextElementSibling.textContent = "Необходимо заполнить «Пароль»"
+    } else if (!regExpPassword.test(password.value)) {
+        password.nextElementSibling.textContent = "Значение «Пароль» должно содержать минимум 6 символов"
+    } else {
+        password.nextElementSibling.textContent = ""
+    }
+}
+
+export function isValidConfirmPassword(password, confirmPassword) {
+    if (confirmPassword.value === "") {
+        confirmPassword.nextElementSibling.textContent = "Необходимо заполнить «Повторите пароль»"
+    } else if (confirmPassword.value !== password.value) {
+        confirmPassword.nextElementSibling.textContent = "Пароли не совпадают"
+    } else {
+        confirmPassword.nextElementSibling.textContent = ""
+    }
+}
